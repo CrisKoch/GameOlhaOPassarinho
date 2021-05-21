00a5e98bf0087d33a273a22ac8bcc4b01cb13d07
CrisKoch <cristinak.iftm@gmail.com>
2021-05-21 18:27:59 -0300

    Query Ranking

--- BackEnd/JOGO-2/JOGO-2/src/main/java/com/kochmann/RankingRepository.java
+++ BackEnd/JOGO-2/JOGO-2/src/main/java/com/kochmann/RankingRepository.java
@@ -2,12 +2,18 @@ package com.kochmann;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RankingRepository extends JpaRepository<Ranking, Integer> {
 
+	@Query(value= "select * from ranking r where r.nivel = ? order by r.pontos desc limit 5", nativeQuery = true)
+	List<Ranking> FindAllAndSort(String nivel);
+	
 	
 
 }
